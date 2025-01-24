eb27fa0432d0de4d00efad4a48f63bce3459b568
Abdu <112977047+abdu61@users.noreply.github.com>
2025-01-25 01:30:46 +0300

    feat: create authentication effects

---
+++ apps/frontend/src/app/state/auth.effects.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
+import { AuthService } from '../service/auth.service';
+import * as AuthActions from './auth.actions';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthEffects {
+  signup$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.signup),
+      switchMap(({ user }) =>
+        this.authService.register(user).pipe(
+          map(() => AuthActions.signupSuccess()),
+          catchError((error) => of(AuthActions.signupFailure({ error })))
+        )
+      )
+    )
+  );
+
+  signin$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.signin),
+      switchMap(({ user }) =>
+        this.authService.login(user).pipe(
+          map((data) => AuthActions.signinSuccess({ accessToken: data.access_token })),
+          catchError((error) => of(AuthActions.signinFailure({ error })))
+        )
+      )
+    )
+  );
+
+  signinSuccess$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.signinSuccess),
+        tap(({ accessToken }) => {
+          localStorage.setItem('access_token', accessToken);
+          this.router.navigate(['/']);
+        })
+      ),
+    { dispatch: false }
+  );
+
+  logout$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.logout),
+        tap(() => {
+          localStorage.removeItem('access_token');
+          this.router.navigate(['/signin']);
+        })
+      ),
+    { dispatch: false }
+  );
+
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+}
