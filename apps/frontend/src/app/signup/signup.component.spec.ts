0a6f7ffc1c6302f6666d45a778476796493f1948
Abdu <112977047+abdu61@users.noreply.github.com>
2025-01-24 23:13:02 +0300

    feat: implement signup form and logic

--- apps/frontend/src/app/signup/signup.component.spec.ts
+++ apps/frontend/src/app/signup/signup.component.spec.ts
@@ -1,21 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { SignupComponent } from './signup.component';
+import { Component } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { User } from '../model/user.model';
+import { Router } from '@angular/router';
 
-describe('SignupComponent', () => {
-  let component: SignupComponent;
-  let fixture: ComponentFixture<SignupComponent>;
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent {
+  user: User = { email: '', password: '' };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [SignupComponent],
-    }).compileComponents();
+  constructor(private authService: AuthService, private router: Router) {}
 
-    fixture = TestBed.createComponent(SignupComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  onSubmit() {
+    this.authService.register(this.user).subscribe({
+      next: () => {
+        this.router.navigate(['/signin']);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+}
