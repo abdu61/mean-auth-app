04549d62cbd6789b72b97a9a9929be66bed5701c
Abdu <112977047+abdu61@users.noreply.github.com>
2025-01-24 22:33:16 +0300

    feat: generate authentication components (signup, signin)

---
+++ apps/frontend/src/app/signin/signin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-signin',
+  imports: [CommonModule],
+  templateUrl: './signin.component.html',
+  styleUrl: './signin.component.css',
+})
+export class SigninComponent {}
