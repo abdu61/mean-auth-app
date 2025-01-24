7feca0b626a6c586897a73f20b82420d2a035c7a
Abdu <112977047+abdu61@users.noreply.github.com>
2025-01-25 01:37:34 +0300

    feat: integrate ngrx store into authentication components

--- apps/frontend/src/app/signin/signin.component.ts
+++ apps/frontend/src/app/signin/signin.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../service/auth.service';
+//import { AuthService } from '../service/auth.service';
 import { User } from '../model/user.model';
-import { Router } from '@angular/router';
+//import { Router } from '@angular/router';
+import { AuthState } from '../state/auth.reducer';
+import { Store } from '@ngrx/store';
+import * as AuthActions from '../state/auth.actions';
 
 @Component({
   selector: 'app-signin',
@@ -11,17 +14,24 @@ import { Router } from '@angular/router';
 export class SigninComponent {
   user: User = { email: '', password: '' };
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private store: Store<AuthState>) {}
 
   onSubmit() {
-    this.authService.login(this.user).subscribe({
-      next: (data) => {
-        localStorage.setItem('access_token', data.access_token);
-        this.router.navigate(['/']);
-      },
-      error: (error) => {
-        console.error(error);
-      },
-    });
+    this.store.dispatch(AuthActions.signin({ user: this.user }));
   }
+  // user: User = { email: '', password: '' };
+
+  // constructor(private authService: AuthService, private router: Router) {}
+
+  // onSubmit() {
+  //   this.authService.login(this.user).subscribe({
+  //     next: (data) => {
+  //       localStorage.setItem('access_token', data.access_token);
+  //       this.router.navigate(['/']);
+  //     },
+  //     error: (error) => {
+  //       console.error(error);
+  //     },
+  //   });
+  // }
 }
--- apps/frontend/src/app/signup/signup.component.ts
+++ apps/frontend/src/app/signup/signup.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { User } from '../model/user.model';
+import { AuthState } from '../state/auth.reducer';
+import { Store } from '@ngrx/store';
+import * as AuthActions from '../state/auth.actions';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +11,12 @@ import { CommonModule } from '@angular/common';
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  user: User = { email: '', password: '' };
+
+  constructor(private store: Store<AuthState>) {}
+
+  onSubmit() {
+    this.store.dispatch(AuthActions.signup({ user: this.user }));
+  }
+}
